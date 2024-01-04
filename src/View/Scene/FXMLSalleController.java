38c6cf3192f44545f6477e7d96fcb84968e533ed
AliceMRCL8 <126554377+AliceMRCL8@users.noreply.github.com>
2024-01-04 16:09:56 +0100

    créations de la fenetre Salle

---
+++ src/View/Scene/FXMLSalleController.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View.Scene;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author Alice
+ */
+public class FXMLSalleController implements Initializable {
+	@FXML
+	private Label titre;
+	@FXML
+	private Button buttonS1;
+	@FXML
+	private Button buttonS2;
+	@FXML
+	private Button buttonS3;
+	@FXML
+	private Button buttonS4;
+	@FXML
+	private Button buttonS5;
+	@FXML
+	private Button buttonS6;
+	@FXML
+	private Button buttonS7;
+	@FXML
+	private Button buttonS8;
+	@FXML
+	private Button buttonS9;
+	@FXML
+	private Button buttonS10;
+	@FXML
+	private Button buttonS11;
+	@FXML
+	private Button buttonS12;
+	@FXML
+	private Button buttonS13;
+	@FXML
+	private Button buttonS14;
+	@FXML
+	private Button buttonS15;
+	@FXML
+	private Button buttonS16;
+	@FXML
+	private Button buttonS17;
+	@FXML
+	private Button buttonS18;
+	@FXML
+	private Button buttonS19;
+	@FXML
+	private Button buttonS20;
+	@FXML
+	private Button buttonS21;
+	@FXML
+	private Button buttonS22;
+	
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
